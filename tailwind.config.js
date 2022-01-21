4b5d59e042ea4970f559c61c79b56b6054aba12a
Zorba11 <zorbathegreek111@gmail.com>
2022-01-21 13:48:35 -0600

    user module, auth-modal component

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: {
+    content: [
+      './src/**/*.{html,ts}'
+    ]
+  },
+  darkMode: false,
+  content: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
