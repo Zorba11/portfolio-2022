9cbc45d4d6a568bc18e93825e125384d01128ef7
Zorba11 <zorbathegreek111@gmail.com>
2022-01-22 11:08:27 -0600

    auth modal, toggle, modal service

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.css'],
+  // providers: [ModalService]
 })
 export class ModalComponent implements OnInit {
+  @Input() modalID = ''
 
-  constructor() { }
+  constructor(public modal: ModalService) {
+
+   }
 
   ngOnInit(): void {
+  }
+
+  closeModal() {
+    this.modal.toggleModal(this.modalID);
   }
 
 }
